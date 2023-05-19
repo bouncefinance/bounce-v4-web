3b3774ae9970c7410837ef63c9291855793d18d8
sz423242122 <sz423242122@gmail.com>
2023-05-19 11:22:21 +0800

    feat: ranklist data change params

--- src/api/game/type.ts
+++ src/api/game/type.ts
@@ -2,8 +2,8 @@ interface RankItem {
   address: string
   avatar: string
   name: string
-  totalCreated: string
-  totalPart: number
+  score: string
+  rank: number
   userId?: string
 }
 export interface IRankResponse {
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -796,12 +796,12 @@ function RankSection({ score }: { score: number | string }) {
     let resultData
     resultData = userRankData?.data
     if (!resultData && list.length > 0) {
-      list.map((item, index) => {
+      list.map(item => {
         if (item.address === account) {
           resultData = {
             address: item.address,
-            rank: index + 1,
-            score: item?.totalCreated
+            rank: item.rank,
+            score: item?.score
           }
         }
       })
@@ -888,7 +888,7 @@ function RankSection({ score }: { score: number | string }) {
                 <RankTopItem
                   userId={rankData.list[1]?.userId || ''}
                   name={rankData.list[1]?.name || shortenAddress(rankData.list[1]?.address) || '--'}
-                  score={`${rankData.list[1]?.totalCreated}SCORE`}
+                  score={`${rankData.list[1]?.score}SCORE`}
                   userIcon={rankData.list[1]?.avatar || UserIcon}
                   isNo1={false}
                   rank={2}
@@ -904,7 +904,7 @@ function RankSection({ score }: { score: number | string }) {
                 <RankTopItem
                   userId={rankData.list[0]?.userId || ''}
                   name={rankData.list[0]?.name || shortenAddress(rankData.list[0]?.address) || '--'}
-                  score={`${rankData.list[0]?.totalCreated}SCORE`}
+                  score={`${rankData.list[0]?.score}SCORE`}
                   userIcon={rankData.list[0]?.avatar || UserIcon}
                   isNo1={true}
                   rank={1}
@@ -914,7 +914,7 @@ function RankSection({ score }: { score: number | string }) {
                 <RankTopItem
                   userId={rankData.list[2]?.userId || ''}
                   name={rankData.list[2]?.name || shortenAddress(rankData.list[2]?.address) || '--'}
-                  score={`${rankData.list[2]?.totalCreated}SCORE`}
+                  score={`${rankData.list[2]?.score}SCORE`}
                   userIcon={rankData.list[2]?.avatar || UserIcon}
                   isNo1={false}
                   rank={3}
@@ -1004,7 +1004,7 @@ function RankSection({ score }: { score: number | string }) {
                         color: '#000'
                       }}
                     >
-                      {item?.totalCreated || '--'} SCORE
+                      {item?.score || '--'} SCORE
                     </Typography>
                   </Box>
                 )
