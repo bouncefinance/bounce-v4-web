17430b376e86709693f7af77eb271b92871111aa
bill <>
2023-05-23 10:27:25 +0800

    feat: update style

--- src/pages/tokenAuction/components/listDialog.tsx
+++ src/pages/tokenAuction/components/listDialog.tsx
@@ -64,7 +64,8 @@ const NFTDialog = styled(Dialog)(({ theme }) => ({
   '.MuiDialog-paper': {
     borderRadius: '30px 30px 0 0',
     backgroundColor: 'var(--ps-text-8)',
-    maxWidth: '100%'
+    maxWidth: '100%',
+    width: '100%'
   }
 }))
 interface TitleProps {
