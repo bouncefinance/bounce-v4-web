d400238c2a34c1b34a9ddae92e68dfe483a0893c
sz423242122 <sz423242122@gmail.com>
2023-06-16 14:18:14 +0800

    feat: add real-world auction product

--- src/pages/realWorldAuction/components/auction.tsx
+++ src/pages/realWorldAuction/components/auction.tsx
@@ -24,6 +24,195 @@ export const marketList: BannerType[] = [
     endTime: ''
   },
   {
+    name: 'Whisky & Whiskey | Rare Finds from Scotland & Kentucky',
+    time: '31 MAY-16 JUNE 2023 10:00 AM EDT',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/25b9495/2147483647/strip/true/crop/4639x2696+0+1358/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2F46%2F82%2F7bdb0bb7469e83fbaf17da8a375d%2F1845-bbo-09d-sothebys-sothebys-23-04-90510-cal.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/whisky-whiskey-rare-finds-from-scotland-kentucky?locale=en&lotFilter=AllLots',
+    startTime: 1685541600,
+    endTime: 1686924000,
+    categories: 'Wines'
+  },
+  {
+    name: 'Liquid jade:Moutai',
+    time: '2-20 JUNE 2023 11:00 AM',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/6887d7d/2147483647/strip/true/crop/3000x1743+0+846/resize/487x283!/format/webp/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2F46%2Fd8%2F392297844d548e7c65ad7c9ab24c%2Fcn0031-93-22a-calendar.jpg',
+    link: 'https://www.sothebys.com/en/articles/liquid-jade-moutai-%E9%87%91%E6%A8%BD%E7%94%98%E9%9C%B2-%E8%B4%B5%E5%B7%9E%E8%8C%85%E5%8F%B0%E7%BD%91%E4%B8%8A%E4%B8%93%E5%9C%BA?locale=en',
+    startTime: 1685718000,
+    endTime: 1687273200,
+    categories: 'Wines'
+  },
+  {
+    name: 'A Life in Art: The Mallin Collection',
+    time: '14 - 21 Jun 2023 10:00 AM EDT',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/0d8b911/2147483647/strip/true/crop/1995x1159+0+446/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2Fdf%2F46%2F0882155641d48de7fbcb80629587%2Fn11283-cc9hm-t2-02-a.jpg',
+    link: 'https://www.sothebys.com/en/auction-catalogue/2023/a-life-in-art-the-mallin-collection-n11283?locale=en&s=intro',
+    startTime: 1686751200,
+    endTime: 1687356000,
+    categories: 'Collectibles'
+  },
+  {
+    name: "Collection Hélène Leloup, Le Journal d'une Pionnière, Vol. I",
+    time: '21 JUNE 2023 5:00 PM CEST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/758b3e4/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2F94%2F2f%2F23041e854158a9254eb81f65aa8e%2Fpf2358-c69km-t1-11-calendar-6x7.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/collection-helene-leloup-journal-dune-pionniere?locale=en',
+    startTime: 1687359600,
+    endTime: 0,
+    categories: 'Collectibles'
+  },
+  {
+    name: 'Streets of Paris',
+    time: '14-21 JUNE 2023 2:00 PM EDT',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/18bac71/2147483647/strip/true/crop/2844x1653+0+1168/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2F50%2F49%2Fc8cc1fbf4e3cad7d10ed014facb1%2Fn11301-ckv8k-t2-02.jpg',
+    link: 'https://www.sothebys.com/en/auction-catalogue/2023/streets-of-paris?locale=en&s=intro',
+    startTime: 1686765600,
+    endTime: 1687370400,
+    categories: 'Art'
+  },
+  {
+    name: 'Une Collection à 360°',
+    time: '22 JUNE 2023 2:00 PM CEST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/22dc02e/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2Fb3%2F6f%2Faf86362a4414876af11e89504368%2Fpf2356-600x700-calendar.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/une-collection-a-360?locale=en',
+    startTime: 1687435200,
+    endTime: 0,
+    categories: 'Collectibles'
+  },
+  {
+    name: 'Vanessa von Zitzewitz, Michael, Intimacy Behind Speed',
+    time: '25 MAY-23 JUNE 2023',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/9de07c6/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2Fc9%2F28%2F7dc6c3bd4761b6909596d4086b6c%2Fmonaco-calendar.jpg',
+    link: 'https://www.sothebys.com/en/digital-catalogues/vanessa-von-zitzewitz-michael-intimacy-behind-speed?locale=en',
+    startTime: 1685005200,
+    endTime: 1687543200,
+    categories: 'Art'
+  },
+  {
+    name: 'Luxury Private Selling Exhibition',
+    time: '25 JANUART -24 JUNE 2023',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/9f28b5f/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2Fdf%2F97%2F0ea20a9147c1a3aa40fdd042a409%2Fdubai-selling-exhb-calendar.jpg',
+    link: 'https://www.sothebys.com/en/digital-catalogues/luxury-private-selling-exhibition?locale=en',
+    startTime: 1674453600,
+    endTime: 1687602600,
+    categories: 'Jewellery'
+  },
+  {
+    name: 'Design 17/20: Silver and Gold Boxes',
+    time: '21-27 JUNE 2023 2:00 PM CEST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/cee044b/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2Fb7%2Ff4%2F7bc561b04b3e9beb5fc3104fc1f1%2Fpf2303-calendar.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/design-17-20-silver-and-gold-boxes-2?locale=en',
+    startTime: 1687348800,
+    endTime: 0,
+    categories: 'Jewellery'
+  },
+  {
+    name: 'Fine Jewels & Watches: Cologne',
+    time: '16-27 JUNE 2023 2:00 PM CEST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/30a0400/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2F46%2F2e%2Fcdec3ce34e088cfd29fbe4ee8e61%2Fde2230-calendar.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/fine-jewels-watches-cologne?locale=en',
+    startTime: 1686916800,
+    endTime: 0,
+    categories: 'Jewellery'
+  },
+  {
+    name: 'The Now Evening Auction',
+    time: '27 JUNE 2023 4:00 PM BST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/c190c04/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2F83%2Ffb%2F4e7e67f844cebce286e94297858a%2Fl23237-600x700-calendar-new.jpg',
+    link: 'https://www.sothebys.com/en/auction-catalogue/2023/the-now-evening-auction-l23237?s=intro&locale=en',
+    startTime: 1687881600,
+    endTime: 0,
+    categories: 'Art'
+  },
+  {
+    name: 'Modern and Contemporary Evening Auction, featuring Face to Face: A Celebration of Portraiture',
+    time: '27 JUNE 2023 5:00 PM BST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/5d59af6/2147483647/strip/true/crop/600x349+0+0/resize/487x283!/format/webp/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2F70%2F2e%2Fb77feb4140cd83082c16651ff4ac%2Fl23006-mod-ctp-eve-webbanners-v4-600x700.jpg',
+    link: 'https://www.sothebys.com/en/auction-catalogue/2023/modern-and-contemporary-evening-auction-featuring-face-to-face-a-celebration-of-portraiture?s=intro&locale=en',
+    startTime: 1687885200,
+    endTime: 0,
+    categories: 'Art'
+  },
+  {
+    name: 'Modern & Contemporary Day Auction',
+    time: '28 JUNE 2023 10:30 AM BST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/cba2bbf/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2F06%2Fec%2Fae92817940018d9abb03e9cc930c%2F231312016-l23007modctpday-webbanners-600x700.jpg',
+    link: 'https://www.sothebys.com/en/auction-catalogue/2023/modern-contemporary-day-auction-l23007?s=intro&locale=en',
+    startTime: 16879482000,
+    endTime: 0,
+    categories: 'Art'
+  },
+  {
+    name: 'Classic Design: Furniture, Silver and Works of Art',
+    time: '28 JUNE 2023 2:30 PM CEST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/dad070c/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2F65%2Fae%2Fbcb6a95945dda28416aa17768b74%2Fpf2311-calendar.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/classic-design?locale=en',
+    startTime: 1687955400,
+    endTime: 0,
+    categories: 'Art'
+  },
+  {
+    name: 'Modern British Art',
+    time: '28 JUNE 2023 2:30 PM BST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/610ed5f/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2F15%2Fad%2F4a87269344df87e40ee61bcf53cd%2Fl23140-600x700-calendar.jpg',
+    link: 'https://www.sothebys.com/en/auction-catalogue/2023/modern-british-art-l23140?s=intro&locale=en',
+    startTime: 1687962600,
+    endTime: 0,
+    categories: 'Art'
+  },
+  {
+    name: 'The Dream Team | Part I',
+    time: '11 MAY-28 JUNE 2023 7:00 PM EDT',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/8196b51/2147483647/strip/true/crop/5081x2953+0+1524/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2F1f%2Fcf%2F866700c24e4f8df6270f01f80be8%2Fmrkt-cjdp6-t3-05.jpg',
+    link: 'https://www.sothebys.com/en/digital-catalogues/the-dream-team-part-i?locale=en',
+    startTime: 1683777600,
+    endTime: 1687993200,
+    categories: 'Fashion'
+  },
+  {
+    name: 'Sports Memorabilia | Part II',
+    time: '20-28 JUNE 2023 7:15 PM EDT',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/ca034f0/2147483647/strip/true/crop/3543x2059+0+742/resize/487x283!/format/webp/quality/90/?url=https%3A%2F%2Fsothebys-md.brightspotcdn.com%2Fbb%2F56%2F2bc77eac4f81a198a27a83968ac9%2Fn11338-cpwjl-t1-01.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/sports-memorabilia-part-ii?locale=en&lotFilter=AllLots',
+    startTime: 1687269600,
+    endTime: 0,
+    categories: 'Fashion'
+  },
+  {
+    name: 'Design 17/20: Furniture and Works of Art',
+    time: '23-29 JUNE 2023 2:00 PM CEST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/7f162f1/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2Fde%2F8e%2Fb309bf4643c78990638ae3887dad%2Fpf2301-calendar.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/design-17-20-furniture-and-works-of-art?locale=en',
+    startTime: 1687525200,
+    endTime: 0,
+    categories: 'Art'
+  },
+  {
+    name: 'Prints & Multiples',
+    time: '22-29 JUNE 2023 3:00 PM CEST',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/5313ef9/2147483647/strip/true/crop/600x349+0+176/resize/487x283!/format/webp/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2F49%2F45%2F20adf45e46f5ad3a15ae9497eb01%2Fde2320-calendar.jpg',
+    link: 'https://www.sothebys.com/en/auction-catalogue/2023/prints-multiples-de2320?s=intro&locale=en',
+    startTime: 1687438800,
+    endTime: 1688043600,
+    categories: 'Art'
+  },
+  {
+    name: 'The Immaculate Leroy Cellar | Flawless Provenance',
+    time: '20-23 JUNE 2023 2:00 PM HKT',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/dbeed8b/2147483647/strip/true/crop/3000x1743+0+846/resize/487x283!/format/webp/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2F09%2F58%2F1e5ab0484831a1b6cc83e7f2bfdd%2Fhk1309-s6-calendar.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/the-immaculate-leroy-cellar-flawless-provenance?locale=en&lotFilter=AllLots',
+    startTime: 1687230000,
+    endTime: 0,
+    categories: 'Wines'
+  },
+  {
+    name: 'Modern & Contemporary Art',
+    time: '2 JULY 2023 3 PM',
+    img: 'https://sothebys-com.brightspotcdn.com/dims4/default/3a00ffc/2147483647/strip/true/crop/3000x1743+0+846/resize/487x283!/format/webp/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2Fa9%2F19%2Fd26a81ec458fb256640716cba4f4%2Fsg7028-cpbgf-02-calendar.jpg',
+    link: 'https://www.sothebys.com/en/buy/auction/2023/modern-contemporary-art?locale=en&lotFilter=AllLots',
+    startTime: 1688281200,
+    endTime: 0,
+    categories: 'Art'
+  },
+  {
     categories: 'Fashion',
     name: 'Handbags Online: The New York Edit',
     time: '30 MAY 10AM - 12 JUN 10AM EDT',
