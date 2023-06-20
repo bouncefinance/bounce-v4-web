d0c82bc3f70e0ed89b3d4cfe42ccb8c3a198a8df
bill <>
2023-06-20 10:25:51 +0800

    feat: update foundoBidDetail test id

--- src/pages/thirdPart/foundoBidDetail.tsx
+++ src/pages/thirdPart/foundoBidDetail.tsx
@@ -311,7 +311,7 @@ const FoundoBidDetail = () => {
 }
 export default function FoundoBidDetailContent() {
   return (
-    <EnglishAuctionValuesProvider backedId={666}>
+    <EnglishAuctionValuesProvider backedId={7396}>
       <FoundoBidDetail />
     </EnglishAuctionValuesProvider>
   )
