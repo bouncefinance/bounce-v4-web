bd0676aef5eed4f91ad96f5826d35d792b93e06d
bill <>
2023-06-21 15:53:05 +0800

    fix: fix spelling

--- src/bounceComponents/auction/HeaderTab/index.tsx
+++ src/bounceComponents/auction/HeaderTab/index.tsx
@@ -63,7 +63,7 @@ const HeaderTab: React.FC<{ onTabChange?: (currentTab: string) => void }> = ({ o
       case 'NFT Auction':
         navigate(routes.nftAuction.index)
         break
-      case 'Real World Capitalize Collectibles Auction':
+      case 'Real World Collectibles Auction':
         navigate(routes.realAuction.index)
         break
       case 'Ads Auction':
