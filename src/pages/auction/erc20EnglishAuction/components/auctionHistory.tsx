b86775c68275a588a8d6b0ea7fafb3c8fed45ab3
kongkong <>
2023-07-14 20:15:18 +0800

    update

--- src/pages/auction/erc20EnglishAuction/components/auctionHistory.tsx
+++ src/pages/auction/erc20EnglishAuction/components/auctionHistory.tsx
@@ -19,7 +19,7 @@ const ActionHistory = ({ poolInfo }: { poolInfo: Erc20EnglishAuctionPoolProp }) 
     poolInfo?.poolId || '',
     poolInfo?.category,
     '',
-    ['Bid']
+    ['Swapped']
   )
   if (!list || (Array.isArray(list.list) && list.list.length === 0)) {
     return null
