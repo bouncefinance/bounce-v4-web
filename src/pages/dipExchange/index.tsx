5470fc7b462e9dba0ea26a89f633157b093156fb
sz423242122 <sz423242122@gmail.com>
2023-07-22 16:22:40 +0800

    test: id

--- src/pages/dipExchange/index.tsx
+++ src/pages/dipExchange/index.tsx
@@ -27,14 +27,14 @@ const DipExchange = () => {
         timaSteamp: item.timaSteamp + oneDay * index,
         active: item.timaSteamp + oneDay * index <= new Date().valueOf(),
         dip: {
-          startAt: 1689924240000,
-          closeAt: 1689924840000,
-          id: 18627
+          startAt: 1690012800000,
+          closeAt: 1690099200000,
+          id: 18657
         },
         dgt: {
-          startAt: 1689924420000,
-          closeAt: 1689925200000,
-          id: 18628
+          startAt: 1690016400000,
+          closeAt: 1690102800000,
+          id: 18658
         }
       }
     })
