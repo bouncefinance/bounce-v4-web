b7150e6c7a270282fdf72521248e655add49f036
sz423242122 <sz423242122@gmail.com>
2023-07-31 15:31:36 +0800

    feat: english auction add connect wallet btn and network btn when status is closed

--- src/pages/auction/erc20EnglishAuction/components/userBlock/instant/index.tsx
+++ src/pages/auction/erc20EnglishAuction/components/userBlock/instant/index.tsx
@@ -1,34 +1,5 @@
-import { Box } from '@mui/material'
-import LeftBox from '../../creatorBlock/left'
-import RightBox from '../right'
-
+import OneTime from '../oneTime'
 const UserBlock = () => {
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexFlow: 'row nowrap',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        paddingTop: '30px'
-      }}
-      gap={'30px'}
-    >
-      <Box
-        sx={{
-          flex: 400
-        }}
-      >
-        <LeftBox />
-      </Box>
-      <Box
-        sx={{
-          flex: 474
-        }}
-      >
-        <RightBox />
-      </Box>
-    </Box>
-  )
+  return <OneTime />
 }
 export default UserBlock
--- src/pages/auction/erc20EnglishAuction/components/userBlock/linear/index.tsx
+++ src/pages/auction/erc20EnglishAuction/components/userBlock/linear/index.tsx
@@ -1,6 +1,5 @@
 import { Box, Grid, Typography } from '@mui/material'
-import LeftBox from '../../creatorBlock/left'
-import RightBox from '../right'
+
 import { PoolStatus } from 'api/pool/type'
 import TokenImage from 'bounceComponents/common/TokenImage'
 import moment from 'moment'
@@ -11,14 +10,63 @@ import ClaimBlock from 'pages/auction/dutchAuction/components/userBlock/claimBlo
 import OthersDetail from '../othersDetail'
 import PoolTextItem from 'pages/auction/dutchAuction/components/poolTextItem'
 import LineClaimChart from './lineClaimChart'
-
+import OneTime from '../oneTime'
+import { useActiveWeb3React } from 'hooks'
+import ConnectWalletButton from 'bounceComponents/fixed-swap/ActionBox/CreatorActionBox/ConnectWalletButton'
+import SwitchNetworkButton from 'bounceComponents/fixed-swap/SwitchNetworkButton'
 const Linear = () => {
   const { data: poolInfo } = useErc20EnglishAuctionPoolInfo()
+  const { account, chainId } = useActiveWeb3React()
+  const isCurrentChainEqualChainOfPool = useMemo(
+    () => chainId === poolInfo?.ethChainId,
+    [chainId, poolInfo?.ethChainId]
+  )
   const isUserJoined = useMemo(
     () => Number(poolInfo?.participant.swappedAmount0),
     [poolInfo?.participant.swappedAmount0]
   )
-  if (poolInfo?.status === PoolStatus.Closed) {
+  const NormalContent = () => {
+    if (!account) {
+      return <ConnectWalletButton />
+    }
+    if (!isCurrentChainEqualChainOfPool && poolInfo) {
+      return <SwitchNetworkButton targetChain={poolInfo?.ethChainId} />
+    }
+    if (!poolInfo) {
+      return <></>
+    }
+    return (
+      <>
+        {isUserJoined && (
+          <Box
+            sx={{
+              width: 'calc(100% - 48px)',
+              display: 'flex',
+              flexFlow: 'column nowrap',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '0 auto',
+              padding: '30px 0 24px'
+            }}
+          >
+            <ClaimBlock
+              isErc20EnglishAuction={true}
+              style={{
+                padding: '0'
+              }}
+              notTimeStyle={{
+                width: '100%',
+                padding: '0 24px',
+                margin: 0
+              }}
+              poolInfo={poolInfo}
+            />
+          </Box>
+        )}
+      </>
+    )
+  }
+  if (poolInfo?.status === PoolStatus.Closed || poolInfo?.status === PoolStatus.Cancelled) {
     return (
       <>
         {/* <Stepper poolInfo={poolInfo} /> */}
@@ -218,64 +266,13 @@ const Linear = () => {
                 </Grid>
               </Grid>
             </Box>
-            {isUserJoined && (
-              <Box
-                sx={{
-                  width: 'calc(100% - 48px)',
-                  display: 'flex',
-                  flexFlow: 'column nowrap',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  margin: '0 auto',
-                  padding: '30px 0 24px'
-                }}
-              >
-                <ClaimBlock
-                  isErc20EnglishAuction={true}
-                  style={{
-                    padding: '0'
-                  }}
-                  notTimeStyle={{
-                    width: '100%',
-                    padding: '0 24px',
-                    margin: 0
-                  }}
-                  poolInfo={poolInfo}
-                />
-              </Box>
-            )}
+            <NormalContent />
           </Box>
         </Box>
         <OthersDetail poolInfo={poolInfo} />
       </>
     )
   }
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexFlow: 'row nowrap',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        paddingTop: '30px'
-      }}
-      gap={'30px'}
-    >
-      <Box
-        sx={{
-          flex: 400
-        }}
-      >
-        <LeftBox />
-      </Box>
-      <Box
-        sx={{
-          flex: 474
-        }}
-      >
-        <RightBox />
-      </Box>
-    </Box>
-  )
+  return <OneTime />
 }
 export default Linear
