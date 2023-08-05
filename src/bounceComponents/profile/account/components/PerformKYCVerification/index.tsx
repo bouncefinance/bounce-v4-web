5092e7320ca8a0f5e79429f3c50f7059816c7e9b
hero <>
2023-08-05 09:49:30 +0800

    feat: update PerformKYCVerification notion

--- src/bounceComponents/profile/account/components/PerformKYCVerification/index.tsx
+++ src/bounceComponents/profile/account/components/PerformKYCVerification/index.tsx
@@ -3,11 +3,12 @@ import { ReactComponent as GreeErrSvg } from 'assets/imgs/icon/gree-err.svg'
 import useBreakpoint from 'hooks/useBreakpoint'
 import SumsubWebDialog from 'bounceComponents/account/SumsubWebDialog'
 import { show } from '@ebay/nice-modal-react'
-import { useMemo } from 'react'
-import { useUserInfo } from 'state/users/hooks'
+import { useCallback, useMemo, useState } from 'react'
+import { useShowLoginModal, useUserInfo } from 'state/users/hooks'
 import { VerifyStatus } from 'api/profile/type'
 import { routes } from 'constants/routes'
 import { IS_TEST_ENV } from '../../../../../constants'
+import CloseIcon from '@mui/icons-material/Close'
 
 const homeShowList = [
   routes.market.index,
@@ -17,25 +18,36 @@ const homeShowList = [
   routes.realAuction.index,
   routes.adsAuction.index
 ]
+
+const sessionStorage_KYC_HIDE_NOTICE = 'sessionStorage_KYC_HIDE_NOTICE'
+
 const accountShowList = Object.values(routes.account)
 const PerformKYCVerification = () => {
+  const [showNotice, setShowNotice] = useState(sessionStorage.getItem(sessionStorage_KYC_HIDE_NOTICE) ? false : true)
+
+  const closeNotice = useCallback(() => {
+    setShowNotice(false)
+    sessionStorage.setItem(sessionStorage_KYC_HIDE_NOTICE, 'hide')
+  }, [])
+
   const isSm = useBreakpoint('sm')
   const { userInfo } = useUserInfo()
-  const isVerify = useMemo(() => userInfo && userInfo.ifKyc === VerifyStatus.NoVerify, [userInfo])
+  const { token } = useUserInfo()
+  const showLoginModal = useShowLoginModal()
+  const isNoVerify = useMemo(() => !userInfo || userInfo.ifKyc === VerifyStatus.NoVerify, [userInfo])
   const { pathname } = window.location
   const isHomeShow = useMemo(() => homeShowList.includes(pathname), [pathname])
   const isAccountShow = useMemo(() => accountShowList.includes(pathname), [pathname])
   const isLg = useBreakpoint('lg')
   return (
     <>
-      {isVerify && (isHomeShow || isAccountShow) && !IS_TEST_ENV && (
+      {isNoVerify && (isHomeShow || isAccountShow) && !IS_TEST_ENV && showNotice && (
         <Box
-          onClick={() => show(SumsubWebDialog)}
           mb={isSm ? 8 : 0}
           sx={{
             background: '#F9FCDE',
             padding: '16px 0',
-            cursor: 'pointer',
+
             marginLeft: isAccountShow && !isLg ? 240 : 'auto',
             '@media(max-width:1296px)': {
               marginBottom: 24,
@@ -46,34 +58,41 @@ const PerformKYCVerification = () => {
           }}
         >
           <Stack
+            direction={'row'}
             sx={{
               maxWidth: '1296px',
               margin: '0 auto',
-              flexDirection: 'row',
-              gap: 16,
-              alignItems: 'center'
+              justifyContent: 'space-between'
             }}
           >
-            <Box>
-              <GreeErrSvg />
-            </Box>
-            <Box
-              sx={{
-                color: '#908E96',
-                '&>span,&': {
-                  fontFamily: 'Inter',
-                  fontSize: '14px',
-                  fontStyle: 'normal',
-                  fontWeight: 400,
-                  lineHeight: '150%'
-                }
-              }}
+            <Stack
+              sx={{ cursor: 'pointer' }}
+              spacing={16}
+              direction={'row'}
+              alignItems={'center'}
+              onClick={() => (!userInfo || !token ? showLoginModal() : show(SumsubWebDialog))}
             >
-              <Typography sx={{ color: '#171717' }} component={'span'}>
-                Perform Sumsub KYC Verification{' '}
-              </Typography>
-              You have not performed KYC verification on the bounce platform yet, Please click to verify.
-            </Box>
+              <GreeErrSvg />
+              <Box
+                sx={{
+                  color: '#908E96',
+                  '&>span,&': {
+                    fontFamily: 'Inter',
+                    fontSize: '14px',
+                    fontStyle: 'normal',
+                    fontWeight: 400,
+                    lineHeight: '150%'
+                  }
+                }}
+              >
+                <Typography sx={{ color: '#171717' }} component={'span'}>
+                  Perform Sumsub KYC Verification{' '}
+                </Typography>
+                You have not performed KYC verification on the bounce platform yet, Please click to verify.
+              </Box>
+            </Stack>
+
+            <CloseIcon sx={{ cursor: 'pointer' }} color="warning" onClick={closeNotice} />
           </Stack>
         </Box>
       )}
