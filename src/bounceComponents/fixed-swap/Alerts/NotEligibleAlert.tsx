dec78bf2d92c0fe50eaef26747948e2f03caef35
sz423242122 <sz423242122@gmail.com>
2023-08-19 16:51:01 +0800

    test: update pool id

--- src/bounceComponents/fixed-swap/Alerts/NotEligibleAlert.tsx
+++ src/bounceComponents/fixed-swap/Alerts/NotEligibleAlert.tsx
@@ -1,7 +1,8 @@
-import { Alert, Typography } from '@mui/material'
+import { Alert, Typography, Button } from '@mui/material'
 import { ReactComponent as ErrorIcon } from 'assets/imgs/icon/err.svg'
 import { routes } from 'constants/routes'
 import { useNavigate } from 'react-router-dom'
+import { useIsMDDown } from 'themes/useTheme'
 import { useRequireWhitelistAndEmail } from 'bounceHooks/auction/useRequireWhitelistAndEmail'
 const NotEligibleAlert = () => {
   const isRequireWhiteListAndEmail = useRequireWhitelistAndEmail()
@@ -13,33 +14,46 @@ const NotEligibleAlert = () => {
       search: `?redirectUrl=${pathname}`
     })
   }
+  const isMd = useIsMDDown()
   const navigate = useNavigate()
   return (
     <Alert
       severity="error"
-      sx={{ borderRadius: 20, '&.MuiAlert-root': { background: '#F9E3DA' } }}
+      sx={{
+        borderRadius: 20,
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        justifyContent: 'flex-start',
+        alignItems: isMd ? 'flex-start' : 'center',
+        '&.MuiAlert-root': { background: '#F9E3DA' }
+      }}
       icon={<ErrorIcon />}
     >
-      <Typography variant="body1" component="span">
-        You are not eligible.&nbsp;{' '}
-      </Typography>
-      <Typography variant="body1" component="span" sx={{ color: '#908E96' }}>
-        You are not whitelisted for this auction.
-      </Typography>
       {isRequireWhiteListAndEmail && (
-        <span
-          style={{
-            cursor: 'pointer',
-            margin: '0 10px',
-            background: 'rgb(212, 245, 222)',
-            lineHeight: '24px',
-            padding: '4px 8px',
-            borderRadius: '24px'
-          }}
-          onClick={() => connectEmail()}
-        >
-          Please connect your email.
-        </span>
+        <>
+          <Typography variant="body1" component="span">
+            You need to connect to your email to participate in this auction
+          </Typography>
+          <Button
+            sx={{
+              margin: '0 10px'
+            }}
+            variant="contained"
+            onClick={() => connectEmail()}
+          >
+            Please connect your email.
+          </Button>
+        </>
+      )}
+      {!isRequireWhiteListAndEmail && (
+        <>
+          <Typography variant="body1" component="span">
+            You are not eligible.&nbsp;{' '}
+          </Typography>
+          <Typography variant="body1" component="span" sx={{ color: '#908E96' }}>
+            You are not whitelisted for this auction.
+          </Typography>
+        </>
       )}
     </Alert>
   )
--- src/bounceHooks/auction/useRequireWhitelistAndEmail.ts
+++ src/bounceHooks/auction/useRequireWhitelistAndEmail.ts
@@ -3,7 +3,8 @@ import { useQueryParams } from 'hooks/useQueryParams'
 import { useMemo } from 'react'
 import { useUserInfo } from 'state/users/hooks'
 // if pool use the whitelist and need to binding email,and then user is lack of email,we use this hook
-const needEmailValidPoolId = [20508]
+// const needEmailValidPoolId = [20508] // stage
+const needEmailValidPoolId = [20510] // alpha
 export const useRequireWhitelistAndEmail = (): boolean => {
   const { sysId } = useQueryParams()
   const { userInfo } = useUserInfo()
