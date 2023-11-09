79c3400bbd5904a3580f9acaaf8676876443e409
sz423242122 <sz423242122@gmail.com>
2023-11-09 12:18:29 +0800

    test: update poolId & sysId

--- src/bounceHooks/auction/useRequireWhitelistAndEmail.ts
+++ src/bounceHooks/auction/useRequireWhitelistAndEmail.ts
@@ -5,15 +5,15 @@ import { useUserInfo } from 'state/users/hooks'
 
 // if pool use the whitelist and need to binding email,and then user is lack of email,we use this hook
 // const needEmailValidPoolId = [20508] // stage
-const needEmailValidPoolId = [20519, 20562, 20698, 20764] // alpha
+const needEmailValidPoolId = [20519, 20562, 20698, 20764, 20916] // alpha
 export const useRequireWhitelistAndEmail = (): boolean => {
   const { poolId, chainShortName, sysId } = useQueryParams()
 
   const { userInfo } = useUserInfo()
   const { account } = useActiveWeb3React()
   const isZetachainathens3 = useMemo(() => {
-    return chainShortName === 'zetachainathens3' && (Number(poolId) === 1 || [20916].indexOf(Number(sysId)) > -1)
-  }, [chainShortName, poolId, sysId])
+    return chainShortName === 'zetachainathens3' && [1, 2].indexOf(Number(poolId)) > -1
+  }, [chainShortName, poolId])
   const isRequireWhiteListAndEmail = useMemo(() => {
     return !!(account && sysId && needEmailValidPoolId.indexOf(Number(sysId)) > -1 && !userInfo?.email)
   }, [account, sysId, userInfo?.email])
@@ -22,8 +22,8 @@ export const useRequireWhitelistAndEmail = (): boolean => {
 export const useIsSpecialPoolId = (): boolean => {
   const { poolId, chainShortName, sysId } = useQueryParams()
   const isZetachainathens3 = useMemo(() => {
-    return chainShortName === 'zetachainathens3' && (Number(poolId) === 1 || [20916].indexOf(Number(sysId)) > -1)
-  }, [chainShortName, poolId, sysId])
+    return chainShortName === 'zetachainathens3' && [1, 2].indexOf(Number(poolId)) > -1
+  }, [chainShortName, poolId])
   const isSpecialPoolId = useMemo(() => {
     return !!(sysId && needEmailValidPoolId.indexOf(Number(sysId)) > -1)
   }, [sysId])
