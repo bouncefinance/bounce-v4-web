fa2fd5a1905d709b7421797dcf249062c47afeaf
sz423242122 <sz423242122@gmail.com>
2023-11-09 19:20:09 +0800

    test: update poolid

--- src/bounceHooks/auction/useRequireWhitelistAndEmail.ts
+++ src/bounceHooks/auction/useRequireWhitelistAndEmail.ts
@@ -5,10 +5,11 @@ import { useUserInfo } from 'state/users/hooks'
 
 // if pool use the whitelist and need to binding email,and then user is lack of email,we use this hook
 // const needEmailValidPoolId = [20508] // stage
-const needEmailValidPoolId = [20519, 20562, 20698, 20764, 20916] // alpha
+const needEmailValidPoolId = [20519, 20562, 20698, 20764, 20916, 20917] // alpha
 /**
  * sysId 20764 & /zetachainathens3/1
  * sysId 20916 & /zetachainathens3/2
+ * sysId 20917 & /zetachainathens3/3
  */
 export const useRequireWhitelistAndEmail = (): boolean => {
   const { poolId, chainShortName, sysId } = useQueryParams()
@@ -16,7 +17,7 @@ export const useRequireWhitelistAndEmail = (): boolean => {
   const { account } = useActiveWeb3React()
   console.log('WhitelistAndEmail >>>', poolId, chainShortName, sysId, userInfo?.email)
   const isZetachainathens3 = useMemo(() => {
-    return chainShortName === 'zetachainathens3' && [1, 2].indexOf(Number(poolId)) > -1
+    return chainShortName === 'zetachainathens3' && [1, 2, 3].indexOf(Number(poolId)) > -1
   }, [chainShortName, poolId])
   const isRequireWhiteListAndEmail = useMemo(() => {
     return !!(account && sysId && needEmailValidPoolId.indexOf(Number(sysId)) > -1 && !userInfo?.email)
@@ -26,7 +27,7 @@ export const useRequireWhitelistAndEmail = (): boolean => {
 export const useIsSpecialPoolId = (): boolean => {
   const { poolId, chainShortName, sysId } = useQueryParams()
   const isZetachainathens3 = useMemo(() => {
-    return chainShortName === 'zetachainathens3' && [1, 2].indexOf(Number(poolId)) > -1
+    return chainShortName === 'zetachainathens3' && [1, 2, 3].indexOf(Number(poolId)) > -1
   }, [chainShortName, poolId])
   const isSpecialPoolId = useMemo(() => {
     return !!(sysId && needEmailValidPoolId.indexOf(Number(sysId)) > -1)
