0acf94a54537c6d15814349464cd1a32f200c7ed
happyjason <>
2023-11-20 10:57:54 +0800

    fix: route name

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -109,7 +109,7 @@ export const routes = {
     CreateProtocolAuction: '/launchpad/create_protocol',
     IphoneAuctionDetail: '/rwa/iphone15',
     FinceptorAuction: '/launchpad/finceptor',
-    launchpadCoin: '/launchpad/cion'
+    launchpadCoin: '/launchpad/coin'
   },
   game: {
     bladeDaoIndex: '/playable/ghositerunner',
