63acacc87dcf5892bcb9853540c39124ed0f6cff
hpy <>
2023-12-11 15:34:49 +0800

    update ai img

--- src/pages/launchpad/PrivatePadDataList.tsx
+++ src/pages/launchpad/PrivatePadDataList.tsx
@@ -47,7 +47,7 @@ import BitStableInfo from './imgs/BitStable-info.png'
 import BitStableLogo from './imgs/Bitstabe-logo.png'
 import DIDNa from './imgs/did-na.png'
 // import GoDIDTokenomics from './imgs/GoDID-Tokenomics.png'
-import AiMeetsBtcImg from './imgs/ai-meets-btc.png'
+import AiMeetsBtcImg from './imgs/ai-meets-btc.jpg'
 import AiMeetsBtsAvatar from './imgs/ai-meets-bts-avatar1.jpg'
 export interface IProjectInfo {
   title: string
