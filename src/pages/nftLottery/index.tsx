7eeadf810b7a74e853b97aabe9e6704a69c14c3e
ppp <>
2023-12-18 11:46:27 +0800

    feat: add TokenInformation section

--- src/pages/nftLottery/index.tsx
+++ src/pages/nftLottery/index.tsx
@@ -1,11 +1,13 @@
 import { Box, Typography } from '@mui/material'
 import SectionA from './sections/sectionA'
+import TokenInformation from './sections/tokenInformation'
 const NftLottery = () => {
   return (
     <Box>
       Nft Lottery
       <Typography variant="lotteryh1">aAAAA</Typography>
       <SectionA />
+      <TokenInformation />
     </Box>
   )
 }
