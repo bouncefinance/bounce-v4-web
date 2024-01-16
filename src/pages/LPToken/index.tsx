3e6958dbcacd21771962aa05e67d2332a3e057d7
Andy <>
2024-01-16 10:55:06 +0800

    update: LP Pool Card Page

--- src/pages/LPToken/index.tsx
+++ src/pages/LPToken/index.tsx
@@ -5,15 +5,20 @@ import { ProjectHead, Tabs } from 'pages/projectIntro'
 import AuctionCard from './auctionCard'
 import LPPoolCard from './components/LPPoolCard'
 import PoolStepper from './components/Stepper'
+import { ChainId } from 'constants/chain'
+import useRandomSelectionLPPoolInfo from 'bounceHooks/auction/useRandomSelectionLPPoolInfo'
+
 const LPToken = () => {
   const item = PrivatePadDataList.find(i => i.keyId === 23) as IPrivatePadProp
+  const _chainId = ChainId.SEPOLIA
+  const { data: poolInfo } = useRandomSelectionLPPoolInfo(_chainId, 21423)
   return (
     <Box>
       <Box>
         <ProjectHead item={item} />
         <AuctionCard />
         <PoolStepper poolInfo={{ openAt: 1705298431, closeAt: 1705298432, claimAt: 1705384831 }} />
-        <LPPoolCard />
+        {poolInfo && <LPPoolCard poolInfo={poolInfo} />}
         <Tabs item={item} />
         <FooterPc />
       </Box>
